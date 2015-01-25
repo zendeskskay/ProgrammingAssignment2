5ed819b7e51936efb1e8b56f7fed3d29486f9a3c
Sarah Kay <skay@zendesk.com>
2015-01-25 11:49:42 -0800

    Added Comments
    added comments to explain process

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,26 @@
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
+makeCacheMatrix <- function(x = matrix()) {  # this function defined the four functions to be used in cacheSolve 
+  m <- NULL                                  # initialize m to Null
+  set <- function(y) {                       # define set function
     x <<- y
     m <<- NULL
   }
-  get <- function() x
-  setinverse <- function(mean) m <<- mean
-  getinverse <- function() m
+  get <- function() x                        # define get function
+  setinverse <- function(mean) m <<- mean    # define setinverse function
+  getinverse <- function() m                 # define getinverse function
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
+  m <- x$getinverse()                 # get inverse of x from parent env. & set m
+  if(!is.null(m)) {                   # if m is not null (i.e. already calculated)
     message("getting cached data")
-    return(m)
+    return(m)                         # return m from cache
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+                          # if m was null, skip to here and run function for inverse
+  data <- x$get()         # pass x into the variable data
+  m <- solve(data, ...)   # solve data to get the inverse of x
+  x$setinverse(m)         # pass the result into m
+  m                       # print m
 }
